48954f4b1cd418446b80fced37a73cc0c2b048a5
Jessie <jessie.2006.14@gmail.com>
2025-07-03 16:13:12 -0700

    Add more change

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Rutas
 const apiRouter = require('./routes/api');  
 app.use('/api', apiRouter);
 
-// Manejo de errores
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
